78b3f94a80d81706a5139b8da48e177e692f447c
Mario Angulo <mario.angulo@mediacurrent.com>
2018-03-08 12:45:35 -0800

    UDEMY-React: Added template variable for challenge

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -4,10 +4,53 @@ console.log("App.js is running!");
 
 // JSX - Javascript XML
 var template = React.createElement(
-  "h1",
-  null,
-  "Indecision App"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Indecision App"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "This is some info"
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Item one"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Item two"
+        )
+    )
+);
+
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Mario Angulo"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: 30"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: Seattle"
+    )
 );
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
